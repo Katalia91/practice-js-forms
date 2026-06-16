0a8a2c30634536b450f5450b15e851388d8dfbc6
Natalia Kur <natalia.kur@yahoo.com>
2026-06-16 14:09:18 +0200

    Refactor task 03

--- 03/app.js
+++ 03/app.js
@@ -3,35 +3,72 @@ const fileInput = document.querySelector("input[type='file']");
 
 fileInput.addEventListener("change", displayImages);
 
-function displayImageDetails(e) {
+function displayImages(e) {
   const files = e.target.files;
-  const fileSize = e.target.files[0].size;
-  const fileSizeInMB = fileSize * 0.000001;
-  for (file of files) {
-    console.log(
-      `File name: ${file.name}, File size: ${(file.size * 0.000001).toFixed(2)}`,
-    );
-    generateImagePreview(e);
+  for (const file of files) {
+    createNewListElement(file);
   }
 }
 
-function generateImagePreview(e) {
+function createNewListElement(file) {
+  const prototypeEl = document.querySelector(".images-list__item--prototype");
+  const clonedPrototypeEl = prototypeEl.cloneNode(true);
+  imagesList.appendChild(clonedPrototypeEl);
+  clonedPrototypeEl.classList.remove("images-list__item--prototype");
+  const fileHeader = clonedPrototypeEl.querySelector(".images-list__item-name");
+  const fileFooter = clonedPrototypeEl.querySelector(".images-list__item-size");
+
+  const fileName = file.name;
+  const fileSize = file.size;
+  const fileSizeinMB = `File size: ${(fileSize * 0.000001).toFixed(2)} MB`;
+  console.log(fileSizeinMB);
+  fileHeader.innerText = fileName;
+  fileFooter.innerText = fileSizeinMB;
   if (file && file.type.includes("image")) {
     const reader = new FileReader();
     reader.onload = function (readerEvent) {
-      const prorotypeEl = document.querySelector(
-        ".images-list__item--prototype",
-      );
-      const prototypeElNew = prorotypeEl.cloneNode(true);
-      imagesList.appendChild(prototypeElNew);
-      const newImg = prototypeElNew.querySelector(".images-list__item-img");
-      newImg.src = readerEvent.target.result;
-      prototypeElNew.classList.remove("images-list__item--prototype");
+      const fileImg = clonedPrototypeEl.querySelector(".images-list__item-img");
+      fileImg.src = readerEvent.target.result;
     };
     reader.readAsDataURL(file);
   }
 }
 
-function displayImages(e) {
-  displayImageDetails(e);
-}
+// function displayImageDetails(e) {
+//   const files = e.target.files;
+//   const fileSize = e.target.files[0].size;
+//   const fileSizeInMB = (fileSize * 0.000001).toFixed(2);
+//   for (const file of files) {
+//     console.log(`File name: ${file.name}, File size: ${fileSizeInMB}`);
+//     generateImagePreview(file);
+//   }
+// }
+
+// function generateImagePreview(file) {
+//   if (file && file.type.includes("image")) {
+//     const reader = new FileReader();
+//     reader.onload = function (readerEvent) {
+//       const prorotypeEl = document.querySelector(
+//         ".images-list__item--prototype",
+//       );
+//       const prototypeElNew = prorotypeEl.cloneNode(true);
+//       imagesList.appendChild(prototypeElNew);
+//       const newImg = prototypeElNew.querySelector(".images-list__item-img");
+//       const imageHeader = prototypeElNew.querySelector(
+//         ".images-list__item-name",
+//       );
+//       const imageFooter = prototypeElNew.querySelector(
+//         ".images-list__item-size",
+//       );
+//       newImg.src = readerEvent.target.result;
+//       imageHeader.innerText = file.name;
+//       imageFooter.innerText = file.size * (0.000001).toFixed(2);
+//       prototypeElNew.classList.remove("images-list__item--prototype");
+//     };
+//     reader.readAsDataURL(file);
+//   }
+// }
+
+// function displayImages(e) {
+//   displayImageDetails(e);
+// }
