cd2dae167b2d0887789128ab3b09abcb7af276e4
Natalia Kur <natalia.kur@yahoo.com>
2026-06-22 21:14:55 +0200

    Complete task 03

--- 03/app.js
+++ 03/app.js
@@ -4,21 +4,24 @@ const chosenFiles = [];
 
 fileInput.addEventListener("change", displayImages);
 
-function isDuplicate(file, chosenFile) {
-  if (file.name === chosenFile.name && file.size === chosenFile.size) {
-    return true;
-  }
-}
-
 function displayImages(e) {
   const files = e.target.files;
   for (const file of files) {
-    chosenFiles.push({
-      name: file.name,
-      size: file.size,
-    });
-    createNewListElement(file);
+    let duplicateFound = false;
+    for (const chosenFile of chosenFiles) {
+      if (file.name === chosenFile.name && file.size === chosenFile.size) {
+        duplicateFound = true;
+      }
+    }
+    if (!duplicateFound) {
+      chosenFiles.push({
+        name: file.name,
+        size: file.size,
+      });
+      createNewListElement(file);
+    }
   }
+
   console.log(chosenFiles);
 }
 
@@ -44,42 +47,3 @@ function createNewListElement(file) {
     reader.readAsDataURL(file);
   }
 }
-
-// function displayImageDetails(e) {
-//   const files = e.target.files;
-//   const fileSize = e.target.files[0].size;
-//   const fileSizeInMB = (fileSize * 0.000001).toFixed(2);
-//   for (const file of files) {
-//     console.log(`File name: ${file.name}, File size: ${fileSizeInMB}`);
-//     generateImagePreview(file);
-//   }
-// }
-
-// function generateImagePreview(file) {
-//   if (file && file.type.includes("image")) {
-//     const reader = new FileReader();
-//     reader.onload = function (readerEvent) {
-//       const prorotypeEl = document.querySelector(
-//         ".images-list__item--prototype",
-//       );
-//       const prototypeElNew = prorotypeEl.cloneNode(true);
-//       imagesList.appendChild(prototypeElNew);
-//       const newImg = prototypeElNew.querySelector(".images-list__item-img");
-//       const imageHeader = prototypeElNew.querySelector(
-//         ".images-list__item-name",
-//       );
-//       const imageFooter = prototypeElNew.querySelector(
-//         ".images-list__item-size",
-//       );
-//       newImg.src = readerEvent.target.result;
-//       imageHeader.innerText = file.name;
-//       imageFooter.innerText = file.size * (0.000001).toFixed(2);
-//       prototypeElNew.classList.remove("images-list__item--prototype");
-//     };
-//     reader.readAsDataURL(file);
-//   }
-// }
-
-// function displayImages(e) {
-//   displayImageDetails(e);
-// }
